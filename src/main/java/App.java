a264d1e6535490142edbe57280920aea7858bfba
craig-ke <lawremce.olonyi.67@gmail.com>
2020-11-23 01:14:30 +0300

    add user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,9 @@
 import java.io.Console;
 
 public class App {
+    public static void main(String[] args) {
+        Console myConsole = System.console();
+        System.out.println("Welcome to ceaser cypher. Do you want to encrypt or decrypt?");
+        String userChoice = myConsole.readLine();
+    }
 }
