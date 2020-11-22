dff3e09dc49ad3b0ff5d4a3effb92f8cbae93ac0
craig-ke <lawremce.olonyi.67@gmail.com>
2020-11-23 01:16:21 +0300

    Add if statement

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,16 @@ public class App {
         Console myConsole = System.console();
         System.out.println("Welcome to ceaser cypher. Do you want to encrypt or decrypt?");
         String userChoice = myConsole.readLine();
+        if (userChoice.equalsIgnoreCase("encrypt")){
+            System.out.println("Enter text to encrypt");
+            String userEncryptionText = myConsole.readLine();
+            System.out.println("Enter shift key");
+            String stringCypherKey = myConsole.readLine();
+            int intCypherKey = Integer.parseInt(stringCypherKey);
+            CeaserCypher ceaserCypher = new CeaserCypher();
+            String encryptedText = ceaserCypher.cypher(userEncryptionText, intCypherKey);
+            System.out.println("Check out your ciphered text below:");
+            System.out.println(encryptedText);
+            System.out.println("-------------------------------------------");
     }
 }
