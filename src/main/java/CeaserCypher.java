f6ad91abe9a8b4dceb15b44782d3819519c98df2
craig-ke <lawremce.olonyi.67@gmail.com>
2020-11-23 01:05:59 +0300

    Seventh test works,add decypher

--- src/main/java/CeaserCypher.java
+++ src/main/java/CeaserCypher.java
@@ -32,5 +32,28 @@ public class CeaserCypher {
         }
         return errorMessage;
     }
+    public String decypher (String encryptionString, int key){
+        String errorMessage = "";
+        if (encryptionString.matches(".*\\d.*") && (key < 1 || key > 25)){
+            errorMessage = "Enter letters only, and the key must between 1 and 25";
+        } else if (encryptionString.matches(".*\\d.*")){
+            errorMessage = "Enter letters only";
+        } else if (key < 1 || key > 25){
+            errorMessage = "The key must between 1 and 25";
+        } else {
+            String cypherText = "";
+            int length = encryptionString.length();
+            for (int i = 0; i < length; i++) {
+                char ch = encryptionString.charAt(i);
+                if (Character.isLetter(ch)) {
+                    if (Character.isUpperCase(ch)) {
+                        char c = (char) (ch - key);
+                        if (c < 'A') {
+                            cypherText += (char) (ch + (26 - key));
+                        }
+                        return cypherText;
+                    }
+                    return errorMessage;
+                }
+            }
 
-}
